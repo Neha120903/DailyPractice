c6767213417c98201583bb40b5d7d599ef739719
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2022-08-24 23:41:09 +0530

    Time: 35 ms (7.09%), Space: 47.5 MB (31.71%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        return (Math.log10(n) / Math.log10(3)) %1 ==0;
+      //  return (Math.log10(n) / Math.log10(3)) %1 ==0;
+        //max int ka log lena h fr 3 ki power m lekr max int possible which is 3 power needs to 
+        //be calculated   
+        //3 ⌊log3​ MaxInt⌋ =3** ⌊19.56⌋ =3 **19  =1162261467
+        return n>0 && 1162261467 %n ==0;
     }
 }
