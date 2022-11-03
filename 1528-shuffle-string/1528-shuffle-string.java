7dd8198428bb21964cbdc111d1b20497fd20dd6e
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2022-11-03 08:27:47 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (95.51%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String restoreString(String s, int[] a) {
+        char ans[] = new char[a.length];
+        for(int i=0; i<a.length;i++)
+        {
+            ans[a[i]]=s.charAt(i);
+        }
+        return String.valueOf(ans);
+    }
+}
